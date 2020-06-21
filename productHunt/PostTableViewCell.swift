1867420571299f82c697a224ce645380ac69c6a6
anikamorris <anikamorris11@gmail.com>
2020-06-21 13:03:19 -0700

    custom class for post cell

---
+++ productHunt/PostTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  PostTableViewCell.swift
+//  productHunt
+//
+//  Created by Anika Morris on 6/21/20.
+//  Copyright © 2020 Anika Morris. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentsCountLabel: UILabel!
+    @IBOutlet weak var votesCountLabel: UILabel!
+    @IBOutlet weak var taglineLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+ 
+    var post: Post? {
+        didSet {
+            // make sure we return if post doesn't exist
+            guard let post = post else { return }
+            // Assign our UI elements to their post counterparts
+            nameLabel.text = post.name
+            taglineLabel.text = post.tagline
+            commentsCountLabel.text = "Comments: \(post.commentsCount)"
+            votesCountLabel.text = "Votes: \(post.votesCount)"
+            updatePreviewImage()
+        }
+    }
+    
+    func updatePreviewImage() {
+       // make sure we return if post doesn't exist
+       guard let post = post else { return }
+       // assign the placeholder image to the UI element
+       productImageView.image = UIImage(named: "placeholder")
+    }
+}
