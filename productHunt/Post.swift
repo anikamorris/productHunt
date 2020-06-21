b73a42d1cc9bd4d2dcd0a7bc3a45c4a103bb40de
anikamorris <anikamorris11@gmail.com>
2020-06-21 12:39:09 -0700

    created Post model

---
+++ productHunt/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  productHunt
+//
+//  Created by Anika Morris on 6/21/20.
+//  Copyright © 2020 Anika Morris. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
