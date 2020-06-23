43045719ac9a5343909c18ff8e944c0276337649
anikamorris <anikamorris11@gmail.com>
2020-06-23 10:23:28 -0700

    display mock comments with navigation

--- productHunt/FeedViewController.swift
+++ productHunt/FeedViewController.swift
@@ -56,8 +56,21 @@ extension FeedViewController: UITableViewDataSource {
 }
 
 extension FeedViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    // provide a fixed size
-    return 250
-  }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        // Get the storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        // Get the commentsView from the storyboard
+        guard let commentsView = storyboard.instantiateViewController(withIdentifier: "commentsView") as? CommentsViewController else {
+            return
+        }
+        // add mock comments
+        commentsView.comments = ["Blah blah blah!", "Good app.", "Wow."]
+        navigationController?.pushViewController(commentsView, animated: true)
+
+    }
 }
